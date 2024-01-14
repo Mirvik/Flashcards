9bedf31c6b6e1b9dae29535dbe7fc5699ebe955b
Mirvik <152074211+Mirvik@users.noreply.github.com>
2024-01-14 21:31:13 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,49 @@
+let num = 0;    
 let isVisible = false;
 let flash_no = 0;
+
+if (localStorage.getItem('cards_num') > 0){
+    for (let i = 1; i < localStorage.length; i++){
+        document.getElementById("flashcards").innerHTML += "<div class='flashcard'><p class='qst'>" + JSON.parse(localStorage.getItem('card'+i)).question + "</p><p class='answ' id='k" + JSON.parse(localStorage.getItem('card'+i)).num + "'>" + JSON.parse(localStorage.getItem('card'+i)).answer + "</p></div>";
+    }
+    
+    let fc = document.getElementsByClassName('flashcard');
+    for(let i = 0; i < fc.length; i++){
+        fc[i].addEventListener("click", function(){
+            let k = document.querySelector("#k" + i);
+            console.log(k);
+            
+            k.classList.toggle('show');
+        });
+    }
+}
 
 function addFlashcard() {
     let question = document.getElementById('question').value;
     let answer = document.getElementById('answer').value;
 
     let flashcards = document.getElementById("flashcards");
-    flashcards.innerHTML += "<div class='flashcard " + flash_no + "'><p class='qst'>" + question + "</p><p class='answ' id='k" + flash_no + "'>" + answer + "</p></div>";
+    flashcards.innerHTML += "<div class='flashcard'><p class='qst'>" + question + "</p><p class='answ' id='k" + flash_no + "'>" + answer + "</p></div>";
+
+    let save = {
+        num: flash_no,
+        question: question,
+        answer: answer
+    }
+
     flash_no+=1;
+    num += 1;
+
+    localStorage.setItem('card' + num, JSON.stringify(save));
+    localStorage.setItem('cards_num', num);
 
     let fc = document.getElementsByClassName('flashcard');
     for(let i = 0; i < fc.length; i++){
         fc[i].addEventListener("click", function(){
-            console.log(document.querySelector("#k" + i));
             let k = document.querySelector("#k" + i);
             console.log(k);
             
-            k.classList.toggle('hide');
+            k.classList.toggle('.show');
         });
     }
 }
@@ -34,5 +61,9 @@ function delCard(){
     let fc = document.getElementsByClassName('flashcard');
     if (fc.length > 0){
         fc[fc.length-1].remove();
+
+        localStorage.removeItem('card' + num)
+        num = num - 1;
+        localStorage.setItem('cards_num', num);
     }
 }
